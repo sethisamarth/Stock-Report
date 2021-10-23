3cc642dd27e1b3795dceebb432cdebd374016ca0
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-24 01:06:48 +0530

    Welcome to Stock Report

---
+++ StockReport/StockReport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stock Report!!!");
+        }
+    }
+}
